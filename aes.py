cd8e452b838dea47cd95f94e85306411c1f4092f
Gushley110 <gushley110@gmail.com>
2020-03-31 00:28:02 -0600

    Finished test

--- aes.py
+++ aes.py
@@ -1,11 +1,27 @@
-import url_to_base64
-
+import url_to_base64 as b64
 from Crypto.Cipher import AES
 from Crypto import Random
 
-key = b'password12345678'
-cipher = AES.new(key,AES.MODE_EAX)
+key = 'password12345678'
+test_string = 'https://open.spotify.com/collection/tracks'
 
-text = cipher.encrypt(b'hola')
+def AESencrypt(key,text):
+    if(len(key) != 16):
+        print('Error')
 
-print(str(text))
+    cipher = AES.new(key.encode(),AES.MODE_EAX)
+    nonce = cipher.nonce
+    text = b64.url_to_base64(text)
+    
+    return (cipher.encrypt(text),nonce)
+
+def AESdecrypt(key,text,nonce):
+    key = key.encode()
+    cipher = AES.new(key,AES.MODE_EAX,nonce)
+    return b64.base64_to_str(cipher.decrypt(text))
+
+cipher_text,nonce = AESencrypt(key,test_string)
+decipher_text = AESdecrypt(key,cipher_text,nonce)
+
+print(cipher_text)
+print(decipher_text)
